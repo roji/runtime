2043f9287171b0ad271a01d7398f2b66652cf9cb
Shay Rojansky <roji@roji.org>
2022-07-11 13:42:26 +0200

    Reenlist

--- src/libraries/System.Transactions.Local/src/System/Transactions/Oletx/OletxTransactionManager.cs
+++ src/libraries/System.Transactions.Local/src/System/Transactions/Oletx/OletxTransactionManager.cs
@@ -537,10 +537,7 @@ namespace System.Transactions.Oletx
             }
 
             // Now ask the resource manager to reenlist.
-            OletxEnlistment returnValue = oletxResourceManager.Reenlist(
-                recoveryInformation.Length,
-                recoveryInformation,
-                enlistmentNotification);
+            OletxEnlistment returnValue = oletxResourceManager.Reenlist(recoveryInformation, enlistmentNotification);
 
             return returnValue;
         }
